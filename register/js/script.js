bd77db131cc7b95e9154320afb4960db2bba7856
bironsecret <slava.v.ivannikvo@gmail.com>
2019-01-19 15:06:40 +0200

    g

--- register/js/script.js
+++ register/js/script.js
@@ -42,7 +42,6 @@ function confirmPasswordEvent() {
 function enableSubmitEvent() {
   $("#submit").prop("disabled", !canSubmit());
   document.cookie = "password" + "=" + $password + ";" + "login" + "=" + $Login + ";" + "email" + "=" + $Email + ";"
-  window.open('file:script.php');
 }
 
 //When event happens on password input
@@ -52,3 +51,28 @@ $password.focus(passwordEvent).keyup(passwordEvent).keyup(confirmPasswordEvent).
 $confirmPassword.focus(confirmPasswordEvent).keyup(confirmPasswordEvent).keyup(enableSubmitEvent);
 
 enableSubmitEvent();
+Даня
+$servername = "sql211.epizy.com";
+$username = "epiz_23276517";
+$password = "1VPu77jR91kK";
+$dbname = "epiz_23276517_userdata";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$cookies = $_COOKIE;
+
+$sql = "INSERT INTO userdata (username, password, email, date)
+VALUES ('John', 'Password123', 'john@example.com', 11-01-2019)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
